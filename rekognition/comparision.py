e743810d5e0c26ce34cd7e4913e23fdd6b4a0660
Mausam-Sharma <35280772+Mausam-Sharma@users.noreply.github.com>
2018-03-16 18:14:11 +0530

    documentation

--- rekognition/comparision.py
+++ rekognition/comparision.py
@@ -2,16 +2,16 @@ import boto3
 import io
 from PIL import Image
 
-rekognition = boto3.client('rekognition', region_name='us-east-1')
+rekognition = boto3.client('rekognition', region_name='us-east-1')    # service clients of AWS boto3
 dynamodb = boto3.client('dynamodb', region_name='us-east-1')
     
-image = Image.open("image.jpg")
+image = Image.open("image.jpg")                           #Image method of PIL library used to convert image into bytestream
 stream = io.BytesIO()
 image.save(stream,format="JPEG")
 image_binary = stream.getvalue()
 
 
-response = rekognition.search_faces_by_image(
+response = rekognition.search_faces_by_image(         #searhes the largest face in an image and compares it to already stored face collection
         CollectionId='class_collection',
         Image={'Bytes':image_binary}                                       
         )
@@ -19,7 +19,7 @@ response = rekognition.search_faces_by_image(
 for match in response['FaceMatches']:
     print (match['Face']['FaceId'],match['Face']['Confidence'])
         
-    face = dynamodb.get_item(
+    face = dynamodb.get_item(                          #retrieval of roll number of faces that have been identified in the image
         TableName='class_collection',  
         Key={'RekognitionId': {'S': match['Face']['FaceId']}}
         )
