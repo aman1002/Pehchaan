fae5299040d6370a073e046aa4a639abb9d13026
Mausam-Sharma <35280772+Mausam-Sharma@users.noreply.github.com>
2018-05-07 12:01:26 +0530

    updated indexer
    now the indexer function indexes photograph by adding more tags such as name, roll number, class, section , etc to a face id

--- lambda/index.py
+++ lambda/index.py
@@ -7,28 +7,31 @@ import urllib
 
 print('Loading function')
 
-dynamodb = boto3.client('dynamodb')                     #aws service clients
+dynamodb = boto3.client('dynamodb')
 s3 = boto3.client('s3')
 rekognition = boto3.client('rekognition')
 
 
-
+# --------------- Helper Functions ------------------
 
 def index_faces(bucket, key):
 
     response = rekognition.index_faces(
         Image={"S3Object":
-            {"Bucket": bucket,              #refer to the event object
+            {"Bucket": bucket,
             "Name": key}},
             CollectionId="class_collection")
     return response
     
-def update_index(tableName,faceId, personroll):
+def update_index(tableName,faceId, personroll,cls,sec,ful):
     response = dynamodb.put_item(
         TableName=tableName,
         Item={
-            'RekognitionId': {'S': faceId},     #storing metadata in nosql table, in our case it is roll number of a particular face
-            'roll_number': {'S': personroll}
+            'RekognitionId': {'S': faceId},
+            'roll_number': {'S': personroll},
+            'full_name': {'S': ful},
+            'class': {'S': cls},
+            'section': {'S': sec}
             }
         ) 
     
@@ -37,23 +40,30 @@ def update_index(tableName,faceId, personroll):
 def lambda_handler(event, context):
 
     # Get the object from the event
-    bucket = event['Records'][0]['s3']['bucket']['name']                            #event generated due to which this function is triggered 
+    bucket = event['Records'][0]['s3']['bucket']['name']
     key = urllib.unquote_plus(
         event['Records'][0]['s3']['object']['key'].encode('utf8'))
 
     try:
 
-             
-        response = index_faces(bucket, key)                         #indexing of faces in the face collection
+        # Calls Amazon Rekognition IndexFaces API to detect faces in S3 object 
+        # to index faces into specified collection
         
-               
+        response = index_faces(bucket, key)
+        
+        # Commit faceId and full name object metadata to DynamoDB
+        
         if response['ResponseMetadata']['HTTPStatusCode'] == 200:
             faceId = response['FaceRecords'][0]['Face']['FaceId']
 
             ret = s3.head_object(Bucket=bucket,Key=key)
             personroll = ret['Metadata']['roll_number']
+            cls = ret['Metadata']['class']
+            sec = ret['Metadata']['section']
+            ful = ret['Metadata']['fullname']
+            
 
-            update_index('class_collection',faceId,personroll)
+            update_index('class_collection',faceId,personroll,cls,sec,ful)
 
         # Print response to console
         print(response)
